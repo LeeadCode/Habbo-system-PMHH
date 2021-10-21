b79b4356cb417be572a3cd6139a60e565f994473
Igor Augusto <igoraugustoramos@gmail.com>
2021-10-20 23:27:48 -0300

    heroku add

--- database/database.js
+++ database/database.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('crud', 'root', '1234',{
-    host: 'localhost',
+const connection = new Sequelize(
+  'heroku_7a5e3f3fb475dd3',
+  'b9c815a6e1d282',
+  'a2de64ea',
+  {
+    host: 'us-cdbr-east-04.cleardb.com',
     dialect: 'mysql'
-})
+  }
+)
+
+// mysql://b9c815a6e1d282:a2de64ea@us-cdbr-east-04.cleardb.com/heroku_7a5e3f3fb475dd3?reconnect=true
 
 module.exports = connection
